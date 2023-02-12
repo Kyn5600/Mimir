a68662eca328935246787c1d0ff022ee2df2668c
Zach Rich <sniperzach075@gmail.com>
2023-02-12 14:30:37 -0500

    First AI Commit

---
+++ main.py
@@ -0,0 +1,66 @@
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+import os
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
+
+os.system("cls")
+# Load the data from a CSV file
+data = pd.read_csv("conversational_english.csv")
+
+# Split the data into training and testing sets
+train_data = data[:int(len(data)*0.8)]
+test_data = data[int(len(data)*0.8):]
+
+# Extract the features from the text data using CountVectorizer
+vectorizer = CountVectorizer()
+train_features = vectorizer.fit_transform(train_data['text'])
+test_features = vectorizer.transform(test_data['text'])
+
+# Train a Multinomial Naive Bayes classifier on the training data
+nb = MultinomialNB()
+nb.fit(train_features, train_data['label'])
+
+# Continuously get user input and make predictions
+while True:
+    user_input = input("Enter a conversational text: ")
+    if user_input == "exit":
+        break
+    input_features = vectorizer.transform([user_input])
+    prediction = nb.predict(input_features)[0]
+    print("Prediction:", prediction)
+    user_confirm = input("Is the prediction correct? (yes/no) ")
+    if user_confirm == "no" or user_confirm == "No" or user_confirm == "n" or user_confirm == "N":
+        if user_input in data['text'].values:
+            continue
+        else:
+            correct_label = input("Enter the correct label: ")
+            data = data.append({'text': user_input, 'label': correct_label}, ignore_index=True)
+            # Re-train the classifier with the updated data
+            train_data = data[:int(len(data)*0.8)]
+            test_data = data[int(len(data)*0.8):]
+            train_features = vectorizer.fit_transform(train_data['text'])
+            test_features = vectorizer.transform(test_data['text'])
+            nb = MultinomialNB()
+            nb.fit(train_features, train_data['label'])
+            # Save the updated data to the CSV file
+            data.to_csv("conversational_english.csv", index=False)
+    if user_confirm == "yes" or user_confirm == "Yes" or user_confirm == "y" or user_confirm == "Y":
+        if user_input in data['text'].values:
+            continue
+        else:
+            correct_label = prediction
+            data = data.append({'text': user_input, 'label': correct_label}, ignore_index=True)
+            # Re-train the classifier with the updated data
+            train_data = data[:int(len(data)*0.8)]
+            test_data = data[int(len(data)*0.8):]
+            train_features = vectorizer.fit_transform(train_data['text'])
+            test_features = vectorizer.transform(test_data['text'])
+            nb = MultinomialNB()
+            nb.fit(train_features, train_data['label'])
+            # Save the updated data to the CSV file
+            data.to_csv("conversational_english.csv", index=False)
+    if user_confirm == "cancel":
+        continue
+    
---
+++ response.py
@@ -0,0 +1,110 @@
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+import random 
+import os
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
+
+os.system("cls")
+# Labels:
+# joke_request joke regque regresponse greeting farewell feeling_question feeling_reponse
+
+# Load the data from the CSV file
+data = pd.read_csv("conversational_english.csv")
+
+# Split the data into training and testing sets
+training_data = data[:int(0.8 * len(data))]
+testing_data = data[int(0.8 * len(data)):]
+
+# Convert the text into numerical feature vectors using CountVectorizer
+vectorizer = CountVectorizer()
+text_features = vectorizer.fit_transform(training_data['text'])
+
+# Train a Naive Bayes classifier on the training data
+classifier = MultinomialNB().fit(text_features, training_data['label'])
+
+correct = ''
+last_input = ''
+other_last_input = ''
+last_label = ''
+name = "Bot:"
+print("Would you like to name me?")
+nameCheck = input("You: ")
+if nameCheck == 'yes':
+    name = input("What is my name? ")
+    name = name + ":"
+elif nameCheck == 'no':
+    print(name,"Okay. Lets continue.")
+# Continuously get user input and generate a response based on the label
+while True:
+    randomNum = random.randint(0,50)
+    user_input = input("You: ")
+    if user_input == "exit":
+        break
+    else:
+        if user_input == 'wrong':
+            new_label = input("Enter the correct label for the user input: ")
+            if other_last_input in data['text'].values:
+                continue
+            else:
+                newest_data = pd.DataFrame({'text': [last_input], 'label': [new_label]})
+                data = data.append(newest_data, ignore_index=True)
+                data.to_csv("conversational_english.csv", index=False)
+        else:
+            user_input_features = vectorizer.transform([user_input])
+            predicted_label = classifier.predict(user_input_features)[0]
+            if other_last_input not in data['text'].values and (other_last_input != '' and other_last_input != ' '):
+                newest_data = pd.DataFrame({'text': [other_last_input], 'label': [last_label]})
+                data = data.append(newest_data, ignore_index=True)
+                data.to_csv("conversational_english.csv", index=False)
+            if predicted_label == 'joke_request':
+                predicted_label = 'joke'
+            if predicted_label == 'feeling_question':
+                predicted_label = 'feeling_response'
+            if predicted_label == 'regque':
+                predicted_label = 'regresponse'
+            if predicted_label == 'fav_col_que':
+                predicted_label = 'fav_color_response'
+            if predicted_label == 'fav_food_que':
+                predicted_label = 'fav_food_response'
+            if predicted_label == 'fav_book_que':
+                predicted_label = 'fav_book_response'
+            if predicted_label == 'fav_movie_que':
+                predicted_label = 'fav_movie_response'
+            if predicted_label == 'travel_que':
+                predicted_label = 'travel_response'
+            if predicted_label == 'guilty_que':
+                predicted_label = 'guilty_response'
+            if predicted_label == 'time_waste_que':
+                predicted_label = 'time_waste_response'
+            if predicted_label == 'celeb_que':
+                predicted_label = 'celeb_response'
+            response = data.loc[data['label'] == predicted_label, 'text'].sample().values[0]
+            print(name, response)
+            last_input = user_input
+            if predicted_label == 'feeling_response' and randomNum>=25:
+                print(name,data.loc[data['label'] == 'feeling_question', 'text'].sample().values[0])
+                new_user_input = input("You: ")
+                if new_user_input == 'wrong':
+                    new_label = input("Enter the correct label for the user input: ")
+                    if user_input in data['text'].values:
+                        continue
+                    else:
+                        newest_data = pd.DataFrame({'text': [last_input], 'label': [new_label]})
+                        data = data.append(newest_data, ignore_index=True)
+                        data.to_csv("conversational_english.csv", index=False)
+                else:
+                    predicted_label = 'feeling_response'
+                    newest_data = pd.DataFrame({'text': [new_user_input], 'label': [predicted_label]})
+                    data = data.append(newest_data, ignore_index=True)
+                    data.to_csv("conversational_english.csv", index=False)
+            other_last_input = last_input
+            last_label = predicted_label
+            if last_label == 'farewell':
+                endCheck = input("Would you like to end this session?")
+                if endCheck == 'yes':
+                    break
+                else:
+                    continue
+                
