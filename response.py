ec2ec4e3b95458e6435928046555b9c6433dc76f
Kyn5600 <sniperzach075@gmail.com>
2023-02-15 09:12:37 -0500

    AI

--- response.py
+++ response.py
@@ -35,7 +35,9 @@ nameCheck = input("You: ")
 if nameCheck == 'yes':
     name = input("What is my name? ")
     name = name + ":"
+    os.system('cls')
 elif nameCheck == 'no':
+    os.system('cls')
     print(name,"Okay. Lets continue.")
 # Continuously get user input and generate a response based on the label
 while True:
@@ -81,9 +83,17 @@ while True:
                 predicted_label = 'time_waste_response'
             if predicted_label == 'celeb_que':
                 predicted_label = 'celeb_response'
+            if predicted_label == 'add':
+                predicted_label = 'add_response'
             response = data.loc[data['label'] == predicted_label, 'text'].sample().values[0]
             print(name, response)
             last_input = user_input
+            if predicted_label == 'add_response':
+                add = input('You:')
+                answer = add.split(' ')
+                add_answer = int(answer[0]) + int(answer[1])
+                print(name, answer[0], '+', answer[1], '=', add_answer)
+                continue
             if predicted_label == 'feeling_response' and randomNum>=25:
                 print(name,data.loc[data['label'] == 'feeling_question', 'text'].sample().values[0])
                 new_user_input = input("You: ")
