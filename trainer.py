f8ea127589ca33a86ccb496a81bb45ec2444e682
Kyn5600 <sniperzach075@gmail.com>
2023-02-25 15:31:00 -0500

    AI

--- trainer.py
+++ trainer.py
@@ -1,29 +1,24 @@
 import pandas as pd
-import numpy as np
-import pickle
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
+import pickle
 
-# Load the CSV data into a pandas dataframe
-data = pd.read_csv('conversational_english.csv')
+# Load data from CSV file
+data = pd.read_csv("conversational_english.csv")
 
-# Separate the text and labels into separate lists
-texts = data['text'].tolist()
-labels = data['label'].tolist()
-
-# Create a CountVectorizer object to tokenize and count the words in the texts
+# Create CountVectorizer object to transform text into feature vectors
 vectorizer = CountVectorizer()
 
-# Fit the vectorizer to the texts to create the vocabulary and transform the texts into a matrix of word counts
-X = vectorizer.fit_transform(texts)
+# Convert text data into feature vectors
+X = vectorizer.fit_transform(data["text"])
 
-# Convert the labels into a numpy array
-y = np.array(labels)
+# Get corresponding labels
+y = data["label"]
 
-# Train a MultinomialNB classifier on the vectorized texts and labels
-nb = MultinomialNB()
-nb.fit(X, y)
+# Train a Multinomial Naive Bayes classifier
+clf = MultinomialNB()
+clf.fit(X, y)
 
-# Dump the trained MultinomialNB classifier and CountVectorizer object to a pickle file
-with open('model.pkl', 'wb') as f:
-    pickle.dump((nb, vectorizer), f)
+# Save the trained model to a pickle file
+with open("new.pkl", "wb") as f:
+    pickle.dump(clf, f)
